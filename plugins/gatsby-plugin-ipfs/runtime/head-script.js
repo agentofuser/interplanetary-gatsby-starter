8315e0b3506d7b69e85bfb57f488d5eaa6ceb6bb
Agent of User <git@agentofuser.com>
2019-06-14 18:19:41 -0400

    feat(codesandbox): tweak to deploy from codesandbox

---
+++ plugins/gatsby-plugin-ipfs/runtime/head-script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+;
+
+(function () {
+  const ipfsPathRegExp = /^(\/(?:ipfs|ipns)\/[^/]+)/;
+  const ipfsPathPrefix = (window.location.pathname.match(ipfsPathRegExp) || [])[1] || '';
+  window.__GATSBY_IPFS_PATH_PREFIX__ = ipfsPathPrefix;
+})();
